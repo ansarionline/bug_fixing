dd8a78fdb1be8b1279e96d5822cd804884166d2b
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-01-05 11:39:47 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from nicegui import ui
+
+@ui.page('/')
+async def main():
+    counter = 0  # This variable is now unique for each client
+
+    def increment():
+        nonlocal counter
+        counter += 1
+        label.text = f'Counter: {counter}'
+
+    label = ui.label(f'Counter: {counter}')
+    ui.button('Increment', on_click=increment)
+
+ui.run()
